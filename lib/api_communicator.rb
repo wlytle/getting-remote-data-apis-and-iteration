4e4e7d56f0bdf5593d5bbf54575f50791c0bea17
wlytle <wlytle89@gmail.com>
2020-10-19 13:56:02 -0700

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,14 +1,26 @@
-require 'rest-client'
-require 'json'
-require 'pry'
+require "rest-client"
+require "json"
+require "pry"
+require "pp"
 
 def get_character_movies_from_api(character_name)
   #make the web request
 
-
-  response_string = RestClient.get('http://swapi.dev/api/people')
+  response_string = RestClient.get("http://swapi.dev/api/people")
   response_hash = JSON.parse(response_string)
 
+  # Get harsh of requested character
+  character_hash = response_hash["results"].find { |result| result["name"] == character_name }
+  #Find all films featuring this character
+  films_urls = character_hash["films"]
+  # Make API requests for each url
+  film_array = []
+  films_urls.each do |film|
+    film = RestClient.get("#{film}")
+    film_array << JSON.parse(film)
+  end
+
+  film_array
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,13 +32,16 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
-def print_movies(films)
+def print_movies(films, character)
+  puts "#{character} appears in these movies:"
   # some iteration magic and puts out the movies in a nice list
+  films.each { |film| puts film["title"] }
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  puts "*" * 30
+  print_movies(films, character)
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the Star Wars API search thing-a-majig"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.strip
 end
